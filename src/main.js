3ebce8c5e11f6a2b546eea51b1405fe21b9756a1
Victoria <vika96karnauhova@gmail.com>
2024-07-03 15:57:22 +0300

    ghjk

---
+++ src/main.js
@@ -0,0 +1,11 @@
+//import    from './js/pixabay-api'
+// Описаний у документації
+import iziToast from 'izitoast';
+// Додатковий імпорт стилів
+import 'izitoast/dist/css/iziToast.min.css';
+
+//import    from './js/render-functions'
+// Описаний у документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
